38ed022736ea270884029125d35f22a36b69801c
Jas Sohi <j450h1@gmail.com>
2020-05-22 05:07:04 -0700

    Successfully run app locally

--- app/run.py
+++ app/run.py
@@ -26,11 +26,13 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+
+engine = create_engine('sqlite:///../data/testDatabase.db')
+df = pd.read_sql_table('test_table', engine)
+
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/pickle_model.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
